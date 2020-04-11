40b7a570e0f408223761db88370a1866ab4839f9
abrahamhope <abrahamisaachope@gmail.com>
2020-04-11 16:31:43 -0500

    added feature, step_definition and runner

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,35 @@
+package com.vytrack.step_definitions;
+
+import com.vytrack.page.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class LoginStepDefinitions {
+    LoginPage loginPage =new LoginPage();
+
+
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+        System.out.println("I am on login page");
+        String URL = ConfigurationReader.getProperty("qa3");
+        Driver.getDriver().get(URL);
+    }
+
+    @When("user logs in as a sales manager")
+    public void user_logs_in_as_a_sales_manager() {
+        System.out.println("I logged in as a sales manager");
+        loginPage.login("salesmanager110","UserUser123");
+    }
+    @Then("user should verify that title is {string}")
+    public void user_should_verify_that_title_is(String string) {
+        System.out.println("I verfied the title is Dashboard");
+        Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
+        Driver.closeDriver();
+    }
+
+
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,7 @@
+Feature: Login
+  As a user, I want to be able to login with username and password
+
+  Scenario: Login as sales manager and verify that title is "Dashboard"
+    Given user is on the landing page
+    When user logs in as a sales manager
+    Then user should verify that title is "Dashboard"
